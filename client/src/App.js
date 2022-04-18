e09257d50c00b884fbd0e5299ff8c70c981dee9b
Sooyoung <sooyoung159@naver.com>
2022-04-18 21:14:42 +0900

    reset.css 제거

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./GlobalStyle";
-import "./reset.css";
 import theme from "./theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div>
-        BASE
-      </div>
+      <div>BASE</div>
     </ThemeProvider>
   );
 }
