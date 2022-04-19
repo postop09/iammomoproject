28d6fe342a72a7d3d12b1f299e7f632822fdb48a
Jupiter-J <namju2912@gmail.com>
2022-04-19 17:06:18 +0900

    newServer

---
+++ server/MoMo/src/main/java/dev/sideproject/momo/MoMoApplication.java
@@ -0,0 +1,13 @@
+package dev.sideproject.momo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoMoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoMoApplication.class, args);
+    }
+
+}
