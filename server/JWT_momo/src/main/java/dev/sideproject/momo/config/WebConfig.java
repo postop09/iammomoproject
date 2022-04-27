fbc27476fcdd0a3946aeb470155a3801c5651dd3
Jupiter-J <namju2912@gmail.com>
2022-04-27 18:53:11 +0900

    edit project

--- server/JWT_momo/src/main/java/dev/sideproject/momo/config/WebConfig.java
+++ server/JWT_momo/src/main/java/dev/sideproject/momo/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://52.79.45.3:8080",
+                .allowedOrigins("http://localhost:8080", //aws 주소 추가
                         "http://localhost:3000",
                         "https://localhost:3000",
                         "https://127.0.0.1:3000",
